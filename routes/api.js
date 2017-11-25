bea653408af9643e55de40a9306889329b81d94f
Mather <409915016@qq.com>
2017-11-25 19:43:12 +0800

    + Creating a React Component.

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const  Ninja = require('../models/ninja');
+const Ninja = require('../models/ninja');
 
+// get a list of ninjas from the db
 router.get('/ninjas', function (req, res, next) {
-  res.send({type: 'GET'})
+  // Ninja.find({}).then(function (ninjas) {
+  //   res.send(ninjas);
+  // })
+  Ninja.geoNear(
+    {
+      type: 'Point',
+      coordinates: [parseFloat(req.query.lng), parseFloat(req.query.lat)]
+    },
+    {maxDistance: 100000, spherical: true}
+  ).then(function (ninjas) {
+    res.send(ninjas)
+  })
 });
 
+//add a new ninja to the db
 router.post('/ninjas', function (req, res, next) {
   Ninja.create(req.body).then(function (ninja) {
     res.send(ninja);
